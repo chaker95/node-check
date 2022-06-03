1f61161a9a543e930172637e145daa48f05dbe82
chaker95 <95969351+chaker95@users.noreply.github.com>
2022-06-03 19:27:14 +0100

    Add files via upload

---
+++ make it modular.js
@@ -0,0 +1,15 @@
+const mymodule = require("./mymodule.js");
+
+const file_path = process.argv[2];
+
+const Extention = process.argv[3];
+
+mymodule(file_path, Extention, (err, data) => {
+    if (err) {
+        return console.error(err);
+    }
+
+    data.forEach((file) => {
+        console.log(file);
+    });
+});
